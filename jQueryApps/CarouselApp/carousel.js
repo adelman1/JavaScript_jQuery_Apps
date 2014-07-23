8190b117c7d841703890481440a2a07939a31244
adelman <adelman@databasaurus.com>
2014-07-23 16:57:32 -0400

    Modified files to reflect exercise 8-3 (not working)

--- jQueryApps/CarouselApp/carousel.js
+++ jQueryApps/CarouselApp/carousel.js
@@ -1,37 +1,35 @@
 $(document).ready(function() {
+	
 	var slider = $("#image_list");
 	var leftProperty, newLeftProperty;
 	
-	//the click event handler for the right button
+	// the click event handler for the right button
 	$("#right_button").click(function() {
-		
 		// get value of current left property
 		leftProperty = parseInt(slider.css("left"));
-		
-		//determine new value of left property
-		if (leftProperty - 300 <= -900) {
+		// determine new value of left property
+		if (leftProperty - 100 <= -900) {
 			newLeftProperty = 0; }
 		else {
-			newLeftProperty = leftProperty - 300; }
-			
+			newLeftProperty = leftProperty - 100; }
 		// use the animate method to change the left property
 		slider.animate( {left: newLeftProperty}, 1000);
-	}); //end click
+	}); // end click
 	
-	//the click event handler for the left button
+	// the click event handler for the left button
 	$("#left_button").click(function() {
-		
 		// get value of current left property
 		leftProperty = parseInt(slider.css("left"));
 		
 		//determine new value of left property
 		if (leftProperty < 0) {
-			newLeftProperty = leftProperty + 300; }
+			newLeftProperty = leftProperty + 100; 
+		}
 		else {
-			newLeftProperty = 0; 
+			newLeftProperty = -800; 
 		}
 			
 		// use the animate method to change the left property
 		slider.animate( {left: newLeftProperty}, 1000);
 	}); // end click
-}); // end ready	
+}); // end ready
