c71ff79340f3bec811f189390813860f16bd105c
adelman <adelman@databasaurus.com>
2014-07-23 16:46:10 -0400

    Added/Modified files for chapter 8 exercises

--- jQueryApps/SlideShowApp/slide_show2.js
+++ jQueryApps/SlideShowApp/slide_show2.js
@@ -4,36 +4,24 @@ $(document).ready(function() {
 	var nextCaption;
 	var nextSlideSource;
 	
-	// the function for running the slide show
-	var runSlideShow = function() {
-		$("#caption").fadeOut(1000);
-		$("#slide").fadeOut(1000,
-			function() {
-				if (nextSlide.next().length == 0) {
-					nextSlide = $("#slides img:first-child");
-				}
-				else {
-					nextSlide = nextSlide.next();
-				}
-				nextSlideSource = nextSlide.attr("src");
-				nextCaption = nextSlide.attr("alt");
-				$("#slide").attr("src", nextSlideSource).fadeIn(1000);
-				$("#caption").text(nextCaption).fadeIn(1000);
-			}
-		)
-	}
-	
-	// start slide show
-	var timer1 = setInterval(runSlideShow, 3000);
-	
-	//starting and stopping slide show
-	$("#slide").hover(
+	// Start slide show
+	timer1 = setInterval(
 		function() {
-			clearInterval(timer1);
-			timer1 = null;
-		},
-		function () {
-			timer1 = setInterval(runSlideShow, 3000);
-		}
-	);
+			$("#caption").fadeOut(2000);
+			$("#slide").slideUp(2000,"linear",
+				function() {
+					if (nextSlide.next().length == 0) {
+						nextSlide = $("#slides img:first-child");
+					}
+					else {
+						nextSlide = nextSlide.next();
+					}
+					nextSlideSource = nextSlide.attr("src");
+					nextCaption = nextSlide.attr("alt");
+					$("#slide").attr("src", nextSlideSource).slideDown(2000, "linear");
+					$("#caption").text(nextCaption).fadeIn(2000);
+				}
+			)
+		}, 
+	5000);
 })
